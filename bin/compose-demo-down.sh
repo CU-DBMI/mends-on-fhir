052abf496c83dd2af8a8101776eaeddae408adc2
Shahim Essaid <shahim@essaid.com>
2023-07-27 20:29:26 -0700

    Docker compose setup.

---
+++ bin/compose-demo-down.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#set -x
+set -e
+set -u
+set -o pipefail
+set -o noclobber
+shopt -s  nullglob
+
+# stack overflow #59895
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+    DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+    SOURCE="$(readlink "$SOURCE")"
+    [[$SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+
+cd "$DIR"/..
+
+docker compose -f compose-demo.yaml down
---
+++ bin/compose-demo-up.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -x
+set -e
+set -u
+set -o pipefail
+set -o noclobber
+shopt -s  nullglob
+
+# stack overflow #59895
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+    DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+    SOURCE="$(readlink "$SOURCE")"
+    [[$SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+
+cd "$DIR"/..
+
+export UID_GID="$(id -u):$(id -g)"
+
+docker compose -f compose-demo.yaml pull
+docker compose \
+  -f compose-demo.yaml \
+  up \
+  --force-recreate
