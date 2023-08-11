16ff2398aa8bf8078c50e8386dde63f23b25770c
Shahim Essaid <shahim@essaid.com>
2023-08-11 06:33:30 -0700

    HAPI compose service.

---
+++ compose/bin/hapi-down.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -x
+set -e
+set -u
+set -o pipefail
+set -o noclobber
+shopt -s  nullglob
+
+# stack overflow #59895
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+    DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+
+cd "$DIR"/..
+
+UID_GID="$(id -u):$(id -g)"
+export UID_GID
+
+docker compose \
+  -f hapi.yaml \
+  down
